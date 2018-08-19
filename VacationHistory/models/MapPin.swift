1acae10e46b2c751818c5a9c8861b6b71e69727d
TimVD <Tim.van.dorpe58@gmail.com>
2018-08-19 18:50:33 +0200

    fixs
    fixes

---
+++ VacationHistory/models/MapPin.swift
@@ -0,0 +1,20 @@
+//
+//  MapPin.swift
+//  VacationHistory
+//
+//  Created by Tim Van Dorpe on 19/08/18.
+//  Copyright © 2018 Tim Van Dorpe. All rights reserved.
+//
+
+import Foundation
+import MapKit
+class MapPin: NSObject, MKAnnotation {
+    var title: String!
+    var subtitle: String?
+    var coordinate: CLLocationCoordinate2D
+    init(title:String, subtitle:String, coordinate:CLLocationCoordinate2D) {
+        self.title = title
+        self.subtitle = subtitle
+        self.coordinate = coordinate
+    }
+}
